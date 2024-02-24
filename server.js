52ca6dd556c1d49434699915c9a50ba1f9a100cc
Ishu Mehta <mehta.ishu14@gmail.com>
2024-02-24 19:11:07 +0530

    configured railway proxy

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 app.use('/api/user', userRouter);
 app.use('/api/task', taskRouter);
 app.get('/', (req, res) => {
@@ -28,43 +26,42 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
 
-const io = require("socket.io")(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: "http://localhost:3000", // (for local machine, also change is SingleTask.js)
-        // credentials: true,
-    },
-});
+// const io = require("socket.io")(server, {
+//     pingTimeout: 60000,
+//     cors: {
+//         origin: "http://localhost:3000", // (for local machine, also change is SingleTask.js)
+//         // credentials: true,
+//     },
+// });
 
-io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-    socket.on("setup", (userData) => {
-        socket.join(userData._id);
-        socket.emit("connected");
-    });
+// io.on("connection", (socket) => {
+//     console.log("Connected to socket.io");
+//     socket.on("setup", (userData) => {
+//         socket.join(userData._id);
+//         socket.emit("connected");
+//     });
 
-    socket.on("join chat", (room) => {
-        socket.join(room);
-        console.log("User Joined Room: " + room);
+//     socket.on("join chat", (room) => {
+//         socket.join(room);
+//         console.log("User Joined Room: " + room);
 
 
-        socket.on("new task", (newTaskReceived) => {
-            // console.log(newMessageRecieved);
-            var task = newTaskReceived.task;
+//         socket.on("new task", (newTaskReceived) => {
+//             // console.log(newMessageRecieved);
+//             var task = newTaskReceived.task;
 
-            if (!chat.sender) return console.log("chat.sender not defined");
+//             if (!chat.sender) return console.log("chat.sender not defined");
 
-            socket.in(chat.sender._id).emit("task recieved", newTaskReceived);
+//             socket.in(chat.sender._id).emit("task recieved", newTaskReceived);
 
-        });
+//         });
 
-        socket.off("setup", () => {
-            console.log("USER DISCONNECTED");
-            socket.leave(userData._id);
-        });
-    });
-});
+//         socket.off("setup", () => {
+//             console.log("USER DISCONNECTED");
+//             socket.leave(userData._id);
+//         });
+//     });
+// });
