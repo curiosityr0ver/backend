7b9892f229c2b0c029d51315df8067f78affb93a
Ishu Mehta <mehta.ishu14@gmail.com>
2024-02-28 15:05:30 +0530

    fixed note routes

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createNoteGroup, allNotes, updateNote, deleteNote } = require('../controller/noteController');
+const { createNote, allNotes, updateNote, deleteNote } = require('../controller/noteController');
 
 
 router.post('/', createNote);
-router.post('/group', createNoteGroup);
 router.get('/', allNotes);
 router.put('/', updateNote);
 router.delete('/:id', deleteNote);
